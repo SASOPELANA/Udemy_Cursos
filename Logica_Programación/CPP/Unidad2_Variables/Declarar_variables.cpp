5fdea1eb22c631afdc9fc6789bc1fb739b62a51f
sergio <sopekof@gmail.com>
2024-03-19 17:57:12 -0300

    19/03/24

---
+++ Logica_Programación/CPP/Unidad2_Variables/Declarar_variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Declarar una variable
+	int entero = 10;
+	float flotante = 7.9;
+	
+	// Imprimimos las variables
+	cout << "Entero: " << entero << ", Flotante: " << flotante << endl;
+	
+	return 0;
+}
+
